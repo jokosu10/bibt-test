d4c9816f1b6c9805daa6e1e9f17c12ae8cdebe45
Joko Susilo <susilo.j8@gmail.com>
2021-08-29 01:09:27 +0700

    example rpc in golang

---
+++ rest-grpc-golang/cmd/run_machine_server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/toransahu/grpc-eg-go/machine"
+	"github.com/toransahu/grpc-eg-go/server"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 9111, "Port on which gRPC server should listern TCP conn")
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	machine.RegisterMachineServer(grpcServer, &server.MachineServer{})
+	grpcServer.Serve(lis)
+	log.Printf("initializing gRPC server on port %d", *port)
+}
