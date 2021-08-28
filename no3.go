a07842bdef22918326864a38623d2d61e51765a2
Joko Susilo <susilo.j8@gmail.com>
2021-08-28 09:20:15 +0700

    Add no3 and no4

---
+++ no3.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func findFirstStringInBracket(str string) string {
+	if len(str) > 0 {
+		indexFirstBracketFound := strings.Index(str, "(")
+		if indexFirstBracketFound >= 0 {
+			runes := []rune(str)
+			wordsAfterFirstBracket := string(runes[indexFirstBracketFound:len(str)])
+			indexClosingBracketFound := strings.Index(wordsAfterFirstBracket, ")")
+			if indexClosingBracketFound >= 0 {
+				runes := []rune(wordsAfterFirstBracket)
+				return string(runes[1 : indexClosingBracketFound-1])
+			} else {
+				return ""
+			}
+		} else {
+			return ""
+		}
+	} else {
+		return ""
+	}
+	return ""
+}
+
+func main() {
+	var a = findFirstStringInBracket("(cccc)")
+	fmt.Println(a)
+}
